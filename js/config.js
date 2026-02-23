00af1d9fc2b7000061767efe849672dd52ec581e
Abby Hatcher <98224330+achatcher@users.noreply.github.com>
2026-02-23 15:55:12 -0500

    updated config.js

--- js/config.js
+++ js/config.js
@@ -1,12 +1,15 @@
 /**
  * SugarBowl Configuration File
- * 
- * This file contains sensitive configuration data.
- * DO NOT commit this file to version control!
  */
 
-// Google Calendar Configuration
 window.SugarBowlConfig = {
     googleCalendarId: 'a399d5825336889640c827ca74c70d3bf0fe29fd70d26fb621d6c526d1443a2b@group.calendar.google.com',
-    apiKey: 'AIzaSyAzeaXGSAcqWDaE1G-L5S-MqFgrthVKkAs'
+    apiKey: 'AIzaSyAzeaXGSAcqWDaE1G-L5S-MqFgrthVKkAs',
+    
+    // ADD THIS SECTION:
+    aws: {
+        bucket: 'sugarbowl-admin-imagesc1ae6-dev',
+        region: 'us-east-2',
+        publicUrl: 'https://sugarbowl-admin-imagesc1ae6-dev.s3.us-east-2.amazonaws.com/public/'
+    }
 };
